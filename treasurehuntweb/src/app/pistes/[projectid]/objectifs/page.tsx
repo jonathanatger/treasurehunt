a91a83a15db54870b2de023308dfe6b5487a03f7
Jonathan ATGER <jonathan.atger@gmail.com>
2024-05-05 21:48:31 +0200

    Added missing argument

--- treasurehuntweb/src/app/pistes/[projectid]/objectifs/page.tsx
+++ treasurehuntweb/src/app/pistes/[projectid]/objectifs/page.tsx
@@ -42,6 +42,7 @@ export default function Page({ params }: { params: { projectid: string } }) {
     setMarkers,
     markerContent,
     areMarkersDraggable,
+    apiImportsAreLoading,
   );
 
   const {
