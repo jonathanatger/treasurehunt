1d7a3c5be9e42413504fb2b7a15dcd44e28b40a8
Jonathan ATGER <jonathan.atger@gmail.com>
2024-06-03 13:27:56 +0200

    updated db schema for many to many relationships, clarification of relations

--- treasurehuntweb/src/server/db/schema.ts
+++ treasurehuntweb/src/server/db/schema.ts
@@ -8,13 +8,14 @@ import {
   integer,
   doublePrecision,
   uuid,
+  primaryKey,
 } from "drizzle-orm/pg-core";
 import { db } from ".";
 import { create } from "domain";
 
 export const createTable = pgTableCreator((name) => `treasurehunt_${name}`);
 
-export const user = createTable("user", {
+export const user = createTable("users", {
   id: uuid("id").primaryKey().unique().defaultRandom(),
   email: text("email").notNull().unique(),
   name: text("name"),
@@ -23,6 +24,12 @@ export const user = createTable("user", {
   updated_at: timestamp("updated_at").default(sql`now()`),
   password: text("password").notNull(),
 });
+
+export const userRelations = relations(user, ({ one, many }) => ({
+  userProjects: many(projects),
+  races: many(raceOnUserJoinTable),
+  teams: many(userOnTeamJoinTable),
+}));
 
 export const projects = createTable("projects", {
   id: serial("id").primaryKey(),
@@ -36,15 +43,12 @@ export const projects = createTable("projects", {
   userEmail: text("email").notNull().unique(),
 });
 
-export const userProjectRelations = relations(user, ({ many }) => ({
-  userProjects: many(projects),
-}));
-
-export const projectUserRelations = relations(projects, ({ one }) => ({
+export const projectRelations = relations(projects, ({ one, many }) => ({
   user: one(user, {
     fields: [projects.userId],
     references: [user.id],
   }),
+  projectObjectives: many(projectObjectives),
 }));
 
 export const projectObjectives = createTable("projectObjectives", {
@@ -58,21 +62,14 @@ export const projectObjectives = createTable("projectObjectives", {
   message: varchar("clue-message"),
 });
 
-export const projectObjectivesRelations = relations(projects, ({ many }) => ({
-  projectObjectives: many(projectObjectives),
+export const objectivesRelations = relations(projectObjectives, ({ one }) => ({
+  project: one(projects, {
+    fields: [projectObjectives.projectid],
+    references: [projects.id],
+  }),
 }));
 
-export const objectivesProjectRelations = relations(
-  projectObjectives,
-  ({ one }) => ({
-    user: one(projects, {
-      fields: [projectObjectives.projectid],
-      references: [projects.id],
-    }),
-  }),
-);
-
-export const track = createTable("tracks", {
+export const race = createTable("races", {
   id: serial("id").primaryKey(),
   projectId: integer("projectId").notNull(),
   createdAt: timestamp("created_at")
@@ -80,67 +77,111 @@ export const track = createTable("tracks", {
     .notNull(),
 });
 
-export const tracksUserRelations = relations(track, ({ many }) => ({
-  usersParticipating: many(user),
-}));
-
-export const userTracksRelation = relations(user, ({ many }) => ({
-  tracksInWhichUserIsParticipating: many(track),
-}));
-
-export const tracksProjectsRelations = relations(track, ({ one }) => ({
+export const raceRelations = relations(race, ({ one, many }) => ({
   relatedProject: one(projects, {
-    fields: [track.projectId],
+    fields: [race.projectId],
     references: [projects.id],
   }),
+  racePositions: many(racePosition),
+  users: many(raceOnUserJoinTable),
 }));
+
+export const raceOnUserJoinTable = createTable(
+  "raceOnUserJoin",
+  {
+    raceId: integer("raceId")
+      .notNull()
+      .references(() => race.id),
+    userId: uuid("userId")
+      .notNull()
+      .references(() => user.id),
+  },
+  (t) => {
+    return { pk: primaryKey({ columns: [t.raceId, t.userId] }) };
+  },
+);
+
+export const raceOnUserJoinRelations = relations(
+  raceOnUserJoinTable,
+  ({ one }) => ({
+    race: one(race, {
+      fields: [raceOnUserJoinTable.raceId],
+      references: [race.id],
+    }),
+    user: one(user, {
+      fields: [raceOnUserJoinTable.userId],
+      references: [user.id],
+    }),
+  }),
+);
 
 export const team = createTable("teams", {
   id: serial("id").primaryKey(),
   name: varchar("name").notNull(),
-  trackId: integer("trackId").notNull(),
+  raceId: integer("raceId").notNull(),
+  racePositionId: integer("racePositionId").notNull(),
 });
 
-export const teamTrackRelations = relations(team, ({ one }) => ({
-  relatedProject: one(track, {
-    fields: [team.trackId],
-    references: [track.id],
+export const teamRelations = relations(team, ({ one, many }) => ({
+  relatedProject: one(race, {
+    fields: [team.raceId],
+    references: [race.id],
+  }),
+  users: many(userOnTeamJoinTable),
+  racePositions: one(racePosition, {
+    fields: [team.racePositionId],
+    references: [racePosition.id],
   }),
 }));
 
-export const teamUserRelations = relations(team, ({ many }) => ({
-  users: many(user),
-}));
+export const userOnTeamJoinTable = createTable(
+  "userOnTeamJoin",
+  {
+    userId: uuid("userId")
+      .notNull()
+      .references(() => user.id),
+    teamId: integer("teamId")
+      .notNull()
+      .references(() => team.id),
+  },
+  (t) => {
+    return { pk: primaryKey({ columns: [t.userId, t.teamId] }) };
+  },
+);
 
-export const trackPosition = createTable("trackPosition", {
+export const userOnTeamJoinRelations = relations(
+  userOnTeamJoinTable,
+  ({ one }) => ({
+    user: one(user, {
+      fields: [userOnTeamJoinTable.userId],
+      references: [user.id],
+    }),
+    team: one(team, {
+      fields: [userOnTeamJoinTable.teamId],
+      references: [team.id],
+    }),
+  }),
+);
+
+export const racePosition = createTable("racePosition", {
   id: serial("id").primaryKey(),
-  trackId: integer("trackId").notNull(),
+  raceId: integer("raceId").notNull(),
   teamId: integer("teamId").notNull(),
   objectiveIndex: integer("userId").notNull(),
   latitude: doublePrecision("latitude").notNull(),
   longitude: doublePrecision("longitude").notNull(),
 });
 
-export const teamTrackPositionRelations = relations(team, ({ one }) => ({
-  users: one(trackPosition, {
-    fields: [team.id],
-    references: [trackPosition.teamId],
+export const racePositionRelations = relations(racePosition, ({ one }) => ({
+  team: one(team, {
+    fields: [racePosition.teamId],
+    references: [team.id],
+  }),
+  race: one(race, {
+    fields: [racePosition.raceId],
+    references: [race.id],
   }),
 }));
-
-export const trackTrackPositionRelations = relations(track, ({ many }) => ({
-  users: many(trackPosition),
-}));
-
-export const trackPositionTrackRelations = relations(
-  trackPosition,
-  ({ one }) => ({
-    users: one(track, {
-      fields: [trackPosition.trackId],
-      references: [track.id],
-    }),
-  }),
-);
 
 export type User = typeof user.$inferSelect;
 export type NewUser = typeof user.$inferInsert;
@@ -151,8 +192,8 @@ export type NewProject = typeof projects.$inferInsert;
 export type ProjectObjective = typeof projectObjectives.$inferSelect;
 export type NewProjectObjective = typeof projectObjectives.$inferInsert;
 
-export type Track = typeof track.$inferSelect;
-export type NewTrack = typeof track.$inferInsert;
+export type Race = typeof race.$inferSelect;
+export type NewRace = typeof race.$inferInsert;
 
 export type Team = typeof team.$inferSelect;
 export type NewTeam = typeof team.$inferInsert;
