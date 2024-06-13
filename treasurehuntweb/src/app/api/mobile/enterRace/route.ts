a4221f4f90c38b982fb108cd22b734a23791919b
Jonathan ATGER <jonathan.atger@gmail.com>
2024-06-13 15:07:08 +0200

    route data type

--- treasurehuntweb/src/app/api/mobile/enterRace/route.ts
+++ treasurehuntweb/src/app/api/mobile/enterRace/route.ts
@@ -3,15 +3,14 @@ import { api } from "~/trpc/server";
 export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
-  const reqData = await request.text();
-  console.log(reqData);
+  const reqData = await request.json();
 
   if (!reqData) return new Response("No code provided", { status: 400 });
 
-  //   const id = await api.races.userJoinsRace({
-  //     code: reqData.data,
-  //     userEmail: reqData.email,
-  //   });
+  const id = await api.races.userJoinsRace({
+    code: reqData.data,
+    userEmail: reqData.email,
+  });
 
-  //   return Response.json({ id });
+  return Response.json({ id });
 }
